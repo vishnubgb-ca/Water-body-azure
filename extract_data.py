acff408773cdf08bdf430a5524320e5f4b0dec7e
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-03-02 22:21:33 +0530

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,19 +1,28 @@
-import boto3
 import os
-
-
-
-import boto3
+from azure.identity import ClientSecretCredential
+from azure.storage.blob import BlobServiceClient
+from datetime import datetime, timedelta
+from azure.storage.blob import generate_blob_sas, BlobSasPermissions
 
 def extract_data():
-
-    s3 = boto3.client('s3')
-    bucket_name = 'deeplearning-mlops'
-    url = s3.generate_presigned_url(
-                    ClientMethod='get_object',
-                    Params={'Bucket': bucket_name, 'Key': 'Water_Bodies_Dataset_Split.zip'},
-                    ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
+    client_id = os.environ["client_id"]
+    client_secret = os.environ["client_secret"]
+    tenant_id = os.environ["tenant_id"]
+    account_url = os.environ["account_url"]
+    default_credential = ClientSecretCredential(client_id=client_id,client_secret=client_secret,tenant_id=tenant_id)
+    blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+    container_client = blob_service_client.get_container_client("deeplearning-mlops")
+    account_key = os.environ["account_key"]
+    permissions = BlobSasPermissions(read=True)
+    sas_token = generate_blob_sas(
+                    account_name=os.environ["account_name"],
+                    container_name="deeplearning-mlops",
+                    blob_name='Water_Bodies_Dataset_Split.zip',
+                    account_key=account_key,  
+                    permission=permissions,
+                    expiry=datetime.utcnow() + timedelta(hours=1),  
                 )
+    url = f"{account_url}/deeplearning-mlops/Water_Bodies_Dataset_Split.zip?{sas_token}"
     print(url)
     return url
 
